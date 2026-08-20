facc8fef35b335dd779f1c53955a3edbf343ccc9
akrithi21115-cell <akrithi21115@gmail.com>
2026-08-20 12:39:54 +0530

    hours

--- src/components/Baddie/Baddie.jsx
+++ src/components/Baddie/Baddie.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react'
 import './Baddie.css'
 
-function getGreeting() {
+
+  function getGreeting() {
   const hour = new Date().getHours()
   if (hour < 12) return 'Good Morning'
-  if (hour > 17) return 'Good Afternoon'
+  if (hour < 17) return 'Good Afternoon'
   return 'Good Evening'
 }
+
 
 function Baddie() {
   const [greeting, setGreeting] = useState(getGreeting())
